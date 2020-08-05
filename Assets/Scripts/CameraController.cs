9ec053511ff10323e839f912838bfe66c50a87fd
Dmitry <bog22232@ya.ru>
2020-08-05 13:48:38 +0300

    Add camera controller, ship controlle upgrade, add texture on water

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _cameraSpeed = 4.0f;
+    [SerializeField] private LayerMask _maskObstacles;
+    private Vector3 _position;
+    private RaycastHit _raycastObstacles;
+    void Start()
+    {
+        _position = _target.InverseTransformPoint(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var preRotation = _target.rotation;
+
+        _target.rotation = Quaternion.Euler(0, preRotation.eulerAngles.y, 0);
+        var currentPosition = _target.TransformPoint(_position);
+
+        transform.position = Vector3.Lerp(transform.position, currentPosition, _cameraSpeed * Time.deltaTime);
+        _target.rotation = preRotation;
+
+        var _currentRotation = Quaternion.LookRotation(_target.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _currentRotation, _cameraSpeed * Time.deltaTime);
+
+        if (Physics.Raycast(_target.position, transform.position - _target.position, out _raycastObstacles,
+            Vector3.Distance(transform.position, _target.position), _maskObstacles))
+        {
+            transform.position = _raycastObstacles.point;
+            transform.LookAt(_target);
+        }
+    }
+}
---
+++ Assets/Scripts/Ship/ShipController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 1.0f;
+    [SerializeField] private float _rateOfTurn = 1.0f;
+    private Rigidbody _rigidbody;
+    private Vector3 _moveDirection = Vector3.zero;
+    private ShipController _ship;
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _ship = GetComponent<ShipController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovementLogic();
+    }
+
+    private void MovementLogic()
+    {
+        _rigidbody.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * _moveSpeed * Time.fixedDeltaTime);
+
+        _rigidbody.AddTorque(new Vector3(0.0f, Input.GetAxis("Horizontal"), 0.0f) * _rateOfTurn * Time.fixedDeltaTime);
+    }
+}
